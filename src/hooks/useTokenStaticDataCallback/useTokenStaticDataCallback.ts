1f68de929e2bfa2b03ddd08607bffe389bd57faa
Tony <neoprince1992@gmail.com>
2022-08-05 10:36:44 +0900

    feat: add old tokenCallback array

--- src/hooks/useTokenStaticDataCallback/useTokenStaticDataCallback.ts
+++ src/hooks/useTokenStaticDataCallback/useTokenStaticDataCallback.ts
@@ -7,6 +7,11 @@ import {
   parseOrder,
   StringAssetType,
 } from 'utils/subgraph';
+import {
+  getTokenStaticCalldata,
+  processTokenStaticCallResults,
+} from 'utils/calls';
+import { useFetchTokenUriCallback } from 'hooks/useFetchTokenUri.ts/useFetchTokenUriCallback';
 import { useActiveWeb3React } from 'hooks/useActiveWeb3React/useActiveWeb3React';
 import { useCallback } from 'react';
 import { Asset, Order } from 'hooks/marketplace/types';
@@ -152,6 +157,61 @@ export const useTokenStaticDataCallbackArray = () => {
 
   return fetchTokenStaticData;
 };
+
+// export const useTokenStaticDataCallback = ({
+//   assetAddress,
+//   assetType,
+// }: TokenStaticCallbackInput) => {
+//   const { chainId } = useActiveWeb3React();
+//   const multi = useMulticall2Contract();
+
+//   const fetchUri = useFetchTokenUriCallback();
+
+//   const fetchTokenStaticData = useCallback(
+//     async (num: number, offset: BigNumber) => {
+//       if (!assetAddress || !assetType || !num) {
+//         return [];
+//       }
+
+//       // just because Indexes can be super huge
+//       const assets: Asset[] = Array.from({ length: num }, (_, i) => {
+//         const x = offset.add(i).toString();
+//         return {
+//           assetId: x,
+//           assetType,
+//           assetAddress,
+//           id: getAssetEntityId(assetAddress, x),
+//         };
+//       });
+
+//       let calls: any[] = [];
+//       assets.map((asset, i) => {
+//         calls = [...calls, ...getTokenStaticCalldata(asset)];
+//       });
+
+//       const results = await tryMultiCallCore(multi, calls);
+
+//       if (!results) {
+//         return [];
+//       }
+
+//       //console.log('yolo tryMultiCallCore res', results);
+//       const staticData = processTokenStaticCallResults(assets, results);
+
+//       const metas = await fetchUri(staticData);
+
+//       return metas.map((x, i) => {
+//         return {
+//           meta: x,
+//           staticData: staticData[i],
+//         };
+//       });
+//     },
+//     [chainId, assetAddress, assetType]
+//   );
+
+//   return fetchTokenStaticData;
+// };
 
 const chooseTokenAssets = (
   assetType: StringAssetType,
